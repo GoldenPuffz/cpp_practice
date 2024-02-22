8920000c09e3b1f27f80ebda37e2aab0a5628df1
GoldenPuffz <vphan004@gmail.com>
2024-02-22 17:34:04 -0500

    alternate hello world with std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello World";
-    return 0;
+#include <iostream> //header file library that let's us use input and outputs
+int main(){//function where everything inside will be executed
+    std::cout << "Hello World"; //The using namespace std line can be omitted and replaced with the std keyword, followed by the :: operator for some objects:
+    return 0;//ends main function
 }
