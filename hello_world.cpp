5a4fb0576387c837cb36b4c41b3cfaeafd521c2c
GoldenPuffz <vphan004@gmail.com>
2024-02-26 14:26:55 -0500

    more examples of C++ io

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello World";
+    cout << "Hello World\n"; //could also use the "endl" operator before the semicolon and after the quote
+    cout << "Next Line World" << endl;
+    cout << "I'm alive" << endl;
+    
+    /*
+variables:
+int
+double 
+char 
+string
+bool : we give them value of true/false, but if we output they say 0 or 1
+*/
+    int myNum = 15;
+    cout << myNum << endl;
+    cout << "I am " << myNum << " days old" << endl;
+
+    int x;
+    cout << "type an integer" << endl;
+    cin >> x;
+    cout << "Your number is " << x << endl;
+    float newNum = 32E2;
+    cout << newNum << endl;
+    char single = 'B'; //must use single quotes or you can use ASCII
+    char a = 65, b = 66, c = 67;
+    cout << a;
+    cout << b;
+    cout << c;
+
+   
+   string statement = "test string";
+   cout << "\n" << statement << endl;
+
+
+   //Operators
+   
+   
+   
+   
+
+
+
+
+
+   
+   
+   
     return 0;
 }
+
